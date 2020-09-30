98d31fcd45d784d74e75e9826c83d660d9adfb8e
Joshua Kennedy <josh@outthinkgroup.com>
2020-09-30 12:54:03 -0400

    migrated db

--- web/src/config.js
+++ web/src/config.js
@@ -3,5 +3,5 @@ export const URL = window.location.origin
 //   URL === 'http://localhost:8910/'
 //     ? 'http://127.0.0.1:4001'
 //     : 'https://afakeartistgoestony.herokuapp.com/'
-// export const WEBSOCKET_URL = 'https://afakeartistgoestony.herokuapp.com/'
-export const WEBSOCKET_URL = 'http://127.0.0.1:4001'
+export const WEBSOCKET_URL = 'https://afakeartistgoestony.herokuapp.com/'
+// export const WEBSOCKET_URL = 'http://127.0.0.1:4001'
